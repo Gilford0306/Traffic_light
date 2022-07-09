0ea59f4c4ffaf7ede62a3772cfe2f2919904ac99
Gilford0306 <Gilford0306@gmail.com>
2022-07-09 10:38:45 +0300

    Add project files.

---
+++ Traffic_light/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_light
+{
+    class Class1
+    {
+        delegate void SayWalkLight(string name);
+        SayWalkLight sayWalkLight;
+
+        void SayRedWalk(string name)
+        {
+            Console.WriteLine($"{name} - Stop");
+        }
+
+        void SayGreenWalk(string name)
+        {
+            Console.WriteLine($"{name} -Go");
+        }
+
+        public Class1(string light = "green")
+        {
+            if (light.ToLower().Equals("red")|| light.ToLower().Equals("yellow"))
+                this.sayWalkLight += SayRedWalk;
+            else
+                this.sayWalkLight += SayGreenWalk;
+        }
+
+
+        public void Run(string name)
+        {
+            //this.sayWalkLight(name);
+            this.sayWalkLight?.Invoke(name);
+        }
+    }
+}
+
---
+++ Traffic_light/Program.cs
@@ -0,0 +1,8 @@
+﻿using Traffic_light;
+
+Class1 myClass = new Class1();
+myClass.Run("Michael");
+Class1 myClass2 = new Class1("Red");
+myClass2.Run("Trevor");
+Class1 myClass3 = new Class1("yeLLow");
+myClass3.Run("Franklin");
